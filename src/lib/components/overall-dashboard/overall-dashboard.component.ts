c451c9beaa55f2f11d712384baa175871ad2c3c9
Adarsha Y H <adarsha.yh@datalyzerint.com>
2025-10-16 12:19:14 +0530

    ggg

---
+++ src/lib/components/overall-dashboard/overall-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-overall-dashboard',
+  imports: [],
+  templateUrl: './overall-dashboard.component.html',
+  styleUrl: './overall-dashboard.component.css'
+})
+export class OverallDashboardComponent {
+
+}
